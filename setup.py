9635d92e5d46b324e16c63d63e4b83aa85dfae18
derekjanni <derekjanni@gmail.com>
2017-06-19 13:25:46 -0700

    more moving things

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zoetrope',
+    version='0.1',
+    description='A templating tool that creates unit tests from an arbitrary file.',
+    packages=find_packages(),
+    scripts=['zoetrope/zoetrope']
+    install_requires=[
+        'mock'
+    ]
+)
