becc150290855a94b841fb29dbc676ef0d6510c9
derekjanni <derekjanni@gmail.com>
2019-09-10 16:14:38 -0700

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='zoetrope',
     version='0.2',
     description='A templating tool that creates unit tests from an arbitrary file.',
-    python_requires='=3.6',
+    python_requires='~=3.6',
     packages=find_packages(),
     scripts=['zoetrope/zoetrope'],
     install_requires=[
